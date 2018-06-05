6c58994ceacf549ffbb36be155351d7385126740
Pedro Luiz Prado Vieira <peluprvi@gmail.com>
2018-06-04 21:21:58 -0300

    feat(text-field): update usage text field example

--- examples/text-fields/usage.vue
+++ examples/text-fields/usage.vue
@@ -1,17 +1,64 @@
 <template>
   <v-form>
     <v-container>
-      <v-layout>
+      <v-layout row wrap>
 
-        <v-flex xs12 md6>
+        <v-flex xs12 sm6 md3>
           <v-text-field
-            label="Label text"
+            label="Regular"
+          ></v-text-field>
+        </v-flex>
+
+        <v-flex xs12 sm6 md3>
+          <v-text-field
+            label="Regular"
             placeholder="Placeholder"
           ></v-text-field>
         </v-flex>
 
-        <v-flex xs12 md6>
-          <v-text-field label="Label text"></v-text-field>
+        <v-flex xs12 sm6 md3>
+          <v-text-field
+            label="Solo"
+            solo
+          ></v-text-field>
+        </v-flex>
+
+        <v-flex xs12 sm6 md3>
+          <v-text-field
+            label="Solo"
+            placeholder="Placeholder"
+            solo
+          ></v-text-field>
+        </v-flex>
+
+        <v-flex xs12 sm6 md3>
+          <v-text-field
+            label="Box"
+            box
+          ></v-text-field>
+        </v-flex>
+
+        <v-flex xs12 sm6 md3>
+          <v-text-field
+            label="Box"
+            placeholder="Placeholder"
+            box
+          ></v-text-field>
+        </v-flex>
+
+        <v-flex xs12 sm6 md3>
+          <v-text-field
+            label="Outline"
+            outline
+          ></v-text-field>
+        </v-flex>
+
+        <v-flex xs12 sm6 md3>
+          <v-text-field
+            label="Outline"
+            placeholder="Placeholder"
+            outline
+          ></v-text-field>
         </v-flex>
 
       </v-layout>
