a4fd4c538f0d2cd9779f84865851d6f0f54dfedc
nekosaur <albert@kaaman.nu>
2018-06-08 21:28:00 +0200

    feature: added internationalization page

--- lang/en/guides/index.js
+++ lang/en/guides/index.js
@@ -1,7 +1,8 @@
 import VueCli3 from './VueCli3'
+import Internationalization from './Internationalization.json'
 import ServerSideRendering from './ServerSideRendering'
 import ALaCarte from './ALaCarte'
 
 export default {
-  VueCli3, ServerSideRendering, ALaCarte
+  VueCli3, Internationalization, ServerSideRendering, ALaCarte
 }
